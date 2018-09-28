3c96c6d6c85e7bbdf5df3e1e7f84980ad931703b
Vitor Luiz Cavalcanti <vitorluizc@outlook.com>
2018-09-28 19:13:46 -0300

    :sparkles: creates Form component

---
+++ src/Form.vue
@@ -0,0 +1,22 @@
+<template>
+  <form>
+    <slot />
+  </form>
+</template>
+
+<script>
+  export default {
+    props: {
+      entries: {
+        type: Array,
+        required: true,
+        validator: (entries) => Array.isArray(entries) && entries.every((name) => typeof value === 'string')
+      },
+      validators: {
+        type: Array,
+        default: () => [],
+        validator: (validators) => Array.isArray(validators) && validators.every((validator) => typeof validator === 'function')
+      }
+    }
+  };
+</script>
