b75af9a7fd580d08deec547dbb299a1bb9ca05e2
Vitor Luiz Cavalcanti <vitorluizc@outlook.com>
2018-11-05 12:16:36 -0200

    :bug: fix non-enumarated read-only property

--- src/object.js
+++ src/object.js
@@ -7,6 +7,8 @@
 export const defineReadOnlyProperty = (target, key, get) => (
   Object.defineProperty(target, key, {
     get,
-    set: () => { throw new Error('Cannot assign to read only property.'); },
+    set: () => { throw new Error(`Cannot assign to readonly property "${key}".`); },
+    enumerable: true,
+    configurable: false
   })
 );
